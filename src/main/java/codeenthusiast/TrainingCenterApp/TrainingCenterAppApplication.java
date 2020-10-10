6e46883aff7a1abc6ca9a435bb003c34ed2d1138
naslakboss <damiannaglak1@gmail.com>
2020-10-10 18:46:39 +0200

    initial commit

---
+++ src/main/java/codeenthusiast/TrainingCenterApp/TrainingCenterAppApplication.java
@@ -0,0 +1,13 @@
+package codeenthusiast.TrainingCenterApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingCenterAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingCenterAppApplication.class, args);
+	}
+
+}

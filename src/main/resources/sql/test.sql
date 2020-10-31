39f4d1a9209f6f0250237d45bcce5c0c11e91606
pangeon <kamil.cecherz@gmail.com>
2020-10-31 14:40:31 +0100

    README.MD 2.0

---
+++ src/main/resources/sql/test.sql
@@ -0,0 +1,12 @@
+CREATE TABLE muscles(
+    muscle_id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(250) NOT NULL,
+    description VARCHAR(500) NOT NULL,
+    is_injured boolean DEFAULT false
+);
+CREATE TABLE images(
+   image_id BIGINT AUTO_INCREMENT PRIMARY KEY,
+   file_path VARCHAR(250) NOT NULL,
+   file_url VARCHAR(250) NOT NULL,
+   muscle_id BIGINT REFERENCES muscles(muscle_id)
+);

bc720c0cd5602890484871b9536c7f4bf69a7a7b
naslakboss <damiannaglak1@gmail.com>
2020-11-01 09:03:23 +0100

    add @restrictions to user fields

--- src/main/java/codeenthusiast/TrainingCenterApp/user/details/UserDetailsDTO.java
+++ src/main/java/codeenthusiast/TrainingCenterApp/user/details/UserDetailsDTO.java
@@ -6,6 +6,7 @@ import codeenthusiast.TrainingCenterApp.constants.Sex;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Range;
 
 @Getter
 @Setter
@@ -14,12 +15,15 @@ public class UserDetailsDTO {
 
     private BodyWeightUnit bodyWeightUnit;
 
+    @Range(min = 0)
     private double weight;
 
     private HeightUnit heightUnit;
 
+    @Range(min = 0)
     private double height;
 
+    @Range(min = 1, max = 120)
     private int age;
 
     private Sex sex;
